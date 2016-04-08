75f9b1e0c9ef201a0c412da3c84e01ac85c44235
Daniel Bider <bider.daniel@gmail.com>
2016-04-08 16:33:34 +0200

    Initial commit Implemented accounting service

---
+++ AccountingService/AccountingService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using MathNet.Numerics.Statistics;
+
+namespace AccountingService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "AccountingService" in both code and config file together.
+    public class AccountingService : IAccountingService
+    {
+        public double GetStandardDeviation(IEnumerable<double> numbers)
+        {
+            if (numbers == null || numbers.Count() == 0) return double.NaN;
+
+            return numbers.StandardDeviation();
+        }
+
+        public double GetMaximum(IEnumerable<double> numbers)
+        {
+            if (numbers == null || numbers.Count() == 0) return double.NaN;
+
+            return numbers.Maximum();
+        }
+        public double GetMinimum(IEnumerable<double> numbers)
+        {
+            if (numbers == null || numbers.Count() == 0) return double.NaN;
+
+            return numbers.Minimum();
+        }
+        public double GetAverage(IEnumerable<double> numbers)
+        {
+            if (numbers == null || numbers.Count() == 0) return double.NaN;
+
+            return numbers.Average();
+        }
+    }
+}
---
+++ AccountingService/IAccountingService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace AccountingService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IAccountingService" in both code and config file together.
+    [ServiceContract]
+    public interface IAccountingService
+    {
+        [OperationContract]
+
+        double GetStandardDeviation(IEnumerable<double> numbers);
+
+        double GetMaximum(IEnumerable<double> numbers);
+
+        double GetMinimum(IEnumerable<double> numbers);
+
+        double GetAverage(IEnumerable<double> numbers);
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "AccountingService.ContractType".
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
